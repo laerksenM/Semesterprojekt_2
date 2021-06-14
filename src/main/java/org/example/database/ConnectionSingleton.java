7a3e7434858448346096093ef705d6b4e9b87b46
juliefalsig <juliefv@me.com>
2021-06-14 13:24:45 +0200

    Singelton

--- src/main/java/org/example/database/ConnectionSingleton.java
+++ src/main/java/org/example/database/ConnectionSingleton.java
@@ -1,10 +1,34 @@
 package org.example.database;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class ConnectionSingleton {
 
+    private String url, user, password;
 
+    private Connection conn = null;
+    private ResultSet resultSet;
+    private Statement statement;
+    private PreparedStatement preparedStatement;
 
+    public ConnectionSingleton(){
+        user="client";
+        password = "1234Hej1234";
+        String schema = "Semesterprojekt 2";
+        url = "jbdc:mysql://"+ "localhost:3306" + schema + "?serverTimezone=Europe/Amsterdam&amp";
+        System.out.println("Connection attemp");
+        try {
+            Class.forName("com.mysql.cj.jbdc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+
+            if (conn != null) {
+                System.out.println("Online");
+            }
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return conn;
+    }
 
 }
+
